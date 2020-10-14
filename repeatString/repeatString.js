c224119d6aee2fd9f191164a79984391bcaed3f4
Josip Matijevic <jspmatijevic@gmail.com>
2020-10-14 22:07:10 +0200

    Test passed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfTimes) {
+    if (numberOfTimes < 0) {
+        return "ERROR";
+    }
+    
+    let repeatedString = "";
+    for (i = 0; i < numberOfTimes; i++) {
+        repeatedString += string;
+      }
+    return repeatedString;
 }
 
 module.exports = repeatString
