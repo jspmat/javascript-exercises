bb496b19b29014f52cfc1466911341894f32ff12
Josip Matijevic <jspmatijevic@gmail.com>
2020-10-14 22:35:40 +0200

    Test passed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -5,7 +5,7 @@ const removeFromArray = function() {
     // first add the element to the output array
     outputArray.push(arguments[0][i]);
     // loop through all the input arguments (except the array) and
-    // remove the element if there is a match
+    // remove the element if there is a match 
     for (let j = 1; j < arguments.length; j++)
     {
       if (arguments[0][i] === arguments[j]) {
