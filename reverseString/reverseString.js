ed4b78f4af9b1b0c6ef242c22d9f7a4484f5be36
Josip Matijevic <jspmatijevic@gmail.com>
2020-10-14 22:13:41 +0200

    Test passed

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversedArray = splitString.reverse();
+    let reversedString = reversedArray.join("");
+    return reversedString;
 }
 
 module.exports = reverseString
