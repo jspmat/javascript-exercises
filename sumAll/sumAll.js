917f62dbdcd8eea76e6e2524061df7fe43bcb4dc
Josip Matijevic <jspmatijevic@gmail.com>
2020-10-15 07:34:24 +0200

    Test passed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+  let sum = 0;
+  
+  if (!Number.isInteger(firstNumber) || 
+      !Number.isInteger(secondNumber) ||
+      firstNumber < 0 ||
+      secondNumber < 0) {
+        return "ERROR";
+      }
 
+  if (firstNumber > secondNumber)
+  {
+    // switch the numbers
+    let tempNumber = firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = tempNumber;
+  }
+
+  for (let i = firstNumber; i <= secondNumber; i++)
+  {
+    sum += i;
+  }
+
+  return sum;
 }
 
 module.exports = sumAll
