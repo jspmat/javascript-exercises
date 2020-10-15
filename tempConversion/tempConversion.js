de3ad37bf1f395925861d4b14c21190e94d41c71
Josip Matijevic <jspmatijevic@gmail.com>
2020-10-15 07:50:55 +0200

    Test passed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9;
 
+  // round it to one decimal if needed
+  celsius = Math.round((celsius + Number.EPSILON) * 10) / 10;
+
+  return celsius;
 }
 
-const ctof = function() {
+const ctof = function(celsius) {
+  let fahrenheit = celsius * 9 / 5 + 32;
 
+  // round it to one decimal if needed
+  fahrenheit = Math.round((fahrenheit + Number.EPSILON) * 10) / 10;
+
+  return fahrenheit;
 }
 
 module.exports = {
