a0436dfcf62460bf670ed3e89441bfd16a339f68
Josip Matijevic <jspmatijevic@gmail.com>
2020-10-17 23:07:21 +0200

    Test passed

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
-
+const palindromes = function(a) {
+  let lowercaseInput = a.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/ /g, '');
+  if (lowercaseInput.split("").reverse().join("") == lowercaseInput) return true;
+  return false;
 }
 
 module.exports = palindromes
