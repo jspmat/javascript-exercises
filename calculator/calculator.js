0ffe9f69c4bb4224a96eb836a279f44fd28bef2a
Josip Matijevic <jspmatijevic@gmail.com>
2020-10-17 22:48:13 +0200

    Test passed

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,32 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (array) {
+	let sum = array.reduce(function(total, element) {
+		return total + element;
+	}, 0);
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let factor = array.reduce(function(total, element) {
+		return total * element;
+	}, 1);
+	return factor;
 }
 
-function power() {
-	
+function power(a, b) {
+	return a**b;
 }
 
-function factorial() {
-	
+function factorial(a) {
+	if (a == 0 || a == 1) return 1;
+	return factorial(a-1) * a;
 }
 
 module.exports = {
