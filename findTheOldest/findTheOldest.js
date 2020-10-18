c70e92d998f0c122169f8974e1459b7a4ce5ff13
Josip Matijevic <jspmatijevic@gmail.com>
2020-10-18 20:57:10 +0200

    Test passed

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+  let oldestPerson = people.reduce(function (oldest, person) {
+    // initialize the output with the first person
+    if (Object.keys(oldest).length === 0 && oldest.constructor === Object) {
+      oldest = person;
+    }
+    
+    // set the year of death to current year if the person is still living
+    let personYearOfDeath = person.yearOfDeath || new Date().getFullYear();
+    let oldestYearOfDeath = oldest.yearOfDeath || new Date().getFullYear();
+    let personAge = personYearOfDeath - person.yearOfBirth;
+    let oldestAge = oldestYearOfDeath - oldest.yearOfBirth;
+    
+    if (personAge > oldestAge) {
+      oldest = person;
+    }
+    return oldest;
+  }, {})
+  return oldestPerson;
 }
 
 module.exports = findTheOldest
